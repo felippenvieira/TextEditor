bfc7c6ed03f5da67358d28fa843f5727fc8fef4d
Felippe <felippe_nvieira@hotmail.com>
2022-02-26 20:11:40 -0300

    Opening files

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -20,7 +20,19 @@ void Menu()
 
 void Open()
 {
+    Console.Clear();
+    Console.WriteLine(" Qual o caminho do arquivo?");
+    string path = Console.ReadLine();
 
+    using (var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    }
+
+    Console.WriteLine();
+    Console.ReadLine();
+    Menu();
 }
 
 void Edit()
@@ -43,7 +55,7 @@ void Edit()
 void Save(string text)
 {
     Console.Clear();
-    Console.WriteLine("Qual caminho para salvar o arquivo?");
+    Console.WriteLine(" Qual o caminho para salvar o arquivo?");
     var path = Console.ReadLine();
 
     using (var file = new StreamWriter(path))
