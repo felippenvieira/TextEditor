b5a681db392e2a3365e25f2e056bfe05f2a647a2
Felippe <felippe_nvieira@hotmail.com>
2022-02-26 20:02:17 -0300

    StreamWriter

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -39,3 +39,15 @@ void Edit()
 
     Console.Write(text);
 }
+
+void Save(string text)
+{
+    Console.Clear();
+    Console.WriteLine("Qual caminho para salvar o arquivo?");
+    var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path))
+    {
+        file.Write(text);
+    }
+}
